d0bffacecf90204652c43c56723938067abaa91f
rua-iri <rory.mcguigan@protonmail.com>
2025-06-29 13:50:52 +0300

    fix: duplicated element

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,31 +4,29 @@ import { Tabs } from "expo-router";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "rgb(36, 219, 36)", headerShown: false }}>
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: "rgba(3, 124, 7, 0.91)",
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="home" color={color} />
-          ),
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "rgb(36, 219, 36)",
+          headerShown: false,
         }}
-      />
-      <Tabs.Screen
-        name="history"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="clock-o" color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color }) => (
+              <FontAwesome size={28} name="home" color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="history"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <FontAwesome size={28} name="clock-o" color={color} />
+            ),
+          }}
+        />
+      </Tabs>
   );
 }
